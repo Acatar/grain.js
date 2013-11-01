9b6413779689c91ecd6e6c2b585be1d71210bb0b
Jason Hansen <jhansen@gmail.com>
2013-11-01 09:22:50 -0400

    added base 64 encoding support to grain.string

--- source/grain.string.js
+++ source/grain.string.js
@@ -139,6 +139,139 @@
             });
         }
 
+        // from http://www.webtoolkit.info/javascript-base64.html
+        var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+        // from http://www.webtoolkit.info/javascript-base64.html
+        $str.utf8Encode = function () {
+            var string = this;
+            string = string.replace(/\r\n/g, "\n");
+            var utftext = "";
+
+            for (var n = 0; n < string.length; n++) {
+
+                var c = string.charCodeAt(n);
+
+                if (c < 128) {
+                    utftext += String.fromCharCode(c);
+                }
+                else if ((c > 127) && (c < 2048)) {
+                    utftext += String.fromCharCode((c >> 6) | 192);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
+                else {
+                    utftext += String.fromCharCode((c >> 12) | 224);
+                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
+
+            }
+            return utftext;
+        }
+
+        // from http://www.webtoolkit.info/javascript-base64.html
+        $str.utf8Decode = function () {
+            var utftext = this;
+            var string = "";
+            var i = 0;
+            var c = c1 = c2 = 0;
+
+            while (i < utftext.length) {
+
+                c = utftext.charCodeAt(i);
+
+                if (c < 128) {
+                    string += String.fromCharCode(c);
+                    i++;
+                }
+                else if ((c > 191) && (c < 224)) {
+                    c2 = utftext.charCodeAt(i + 1);
+                    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                    i += 2;
+                }
+                else {
+                    c2 = utftext.charCodeAt(i + 1);
+                    c3 = utftext.charCodeAt(i + 2);
+                    string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                    i += 3;
+                }
+
+            }
+
+            return string;
+        }
+
+        // from http://www.webtoolkit.info/javascript-base64.html
+        $str.base64Encode = function () {
+            var input = this;
+            var output = "";
+            var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+            var i = 0;
+ 
+            input = input.Utf8Encode();
+ 
+            while (i < input.length) {
+ 
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+ 
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+ 
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+ 
+                output = output +
+			    _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+			    _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+ 
+            }
+ 
+            return output;
+        }
+
+        // from http://www.webtoolkit.info/javascript-base64.html
+        $str.base64Decode = function () {
+            var input = this;
+            var output = "";
+            var chr1, chr2, chr3;
+            var enc1, enc2, enc3, enc4;
+            var i = 0;
+
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            while (i < input.length) {
+
+                enc1 = _keyStr.indexOf(input.charAt(i++));
+                enc2 = _keyStr.indexOf(input.charAt(i++));
+                enc3 = _keyStr.indexOf(input.charAt(i++));
+                enc4 = _keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+            }
+
+            output = output.Utf8Decode();
+            return output;
+        }
+
         //====================================================================================================================
         //====================================================================================================================
         // JavaScript String Extensions
@@ -188,6 +321,34 @@
         if (!String.Replace)                                                           // extend String with the static Remove method
             String.Replace = $str.replace;
 
-        return $str;      
+        // Extend String for Base64 Encoding
+        if (!String.prototype.Base64Encode)
+            String.prototype.Base64Encode = $str.base64Encode;
+
+        if (!String.Base64Encode)
+            String.Base64Encode = $str.base64Encode;
+
+        // Extend String for Base64 Decoding
+        if (!String.prototype.Base64Decode)
+            String.prototype.Base64Decode = $str.base64Decode;
+
+        if (!String.Base64Decode)
+            String.Base64Decode = $str.base64Decode;
+
+        // Extend String for Utf8 Encoding
+        if (!String.prototype.Utf8Encode)
+            String.prototype.Utf8Encode = $str.utf8Encode;
+
+        if (!String.Utf8Encode)
+            String.Utf8Encode = $str.utf8Encode;
+
+        // Extend String for Utf8 Decoding
+        if (!String.prototype.Utf8Decode)
+            String.prototype.Utf8Decode = $str.utf8Decode;
+
+        if (!String.Utf8Decode)
+            String.Utf8Decode = $str.utf8Decode;
+
+        return $str;
     });
 })(window.jQuery, window, window.grain);
